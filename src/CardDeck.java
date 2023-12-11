25ac78f20867f68f4abdca501a57679d345302ae
Asxchksel <axel.mannheimer@elev.ga.ntig.se>
2023-12-11 09:42:06 +0100

    random card given

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 public class CardDeck {
-    private int cardCount = 52;
+    private ArrayList <String> cardRank = new ArrayList<>();
+
+    private ArrayList <String> cardSuit = new ArrayList<>();
 
 
+
+    public CardDeck(){}
+
+    public void drawCard(){
+        Random rand = new Random();
+        int randomCard = rand.nextInt( 1,14 );
+
+        int suit = rand.nextInt(1, 5);
+
+        System.out.println(randomCard);
+
+    }
 }
--- src/Players.java
+++ src/Players.java
@@ -1,3 +1,10 @@
 public class Players {
+private int cards;
+private int maxCards = 5;
+private int cardValue;
+
+public Players(){}
+
+
 
 }
